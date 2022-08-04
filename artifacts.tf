c12bb6fc7c737caf7b41306680ba7db582e79d2e
Brad Sickles <brad.sickles@gmail.com>
2022-08-04 13:28:17 -0400

    Remove compliance mode, add scheme to public urls

--- artifacts.tf
+++ artifacts.tf
@@ -9,17 +9,6 @@ resource "aws_s3_bucket" "artifacts" {
   object_lock_enabled = true
 }
 
-resource "aws_s3_bucket_object_lock_configuration" "artifacts" {
-  bucket = aws_s3_bucket.artifacts.bucket
-
-  rule {
-    default_retention {
-      mode = "COMPLIANCE"
-      days = 5
-    }
-  }
-}
-
 resource "aws_s3_bucket_acl" "artifacts" {
   bucket = aws_s3_bucket.artifacts.id
   acl    = "private"
--- urls.tf
+++ urls.tf
@@ -3,7 +3,11 @@ locals {
   // Typically, they are created through capabilities attached to the application
   // If this module has URLs, add them here as list(string)
   additional_private_urls = []
-  additional_public_urls  = [aws_elastic_beanstalk_environment.this.endpoint_url]
+
+  additional_public_urls  = [
+    "http://${aws_elastic_beanstalk_environment.this.cname}",
+    "http://${aws_elastic_beanstalk_environment.this.endpoint_url}",
+  ]
 
   public_urls  = compact(concat([for url in try(local.capabilities.public_urls, []) : url["url"]], local.additional_public_urls))
   private_urls = compact(concat([for url in try(local.capabilities.private_urls, []) : url["url"]], local.additional_private_urls))
