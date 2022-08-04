766363574bf7e7e87ee434cf223916292974869d
Brad Sickles <brad.sickles@gmail.com>
2022-08-04 10:19:33 -0400

    Remove empty urls from outputs

--- outputs.tf
+++ outputs.tf
@@ -45,20 +45,12 @@ output "environment_id" {
   description = "string ||| The ID of the this Elastic Beanstalk Environment"
 }
 
-locals {
-  // Private and public URLs are shown in the Nullstone UI
-  // Typically, they are created through capabilities attached to the application
-  // If this module has URLs, add them here as list(string) 
-  additional_private_urls = []
-  additional_public_urls  = [aws_elastic_beanstalk_environment.this.endpoint_url]
-}
-
 output "private_urls" {
-  value       = concat([for url in try(local.capabilities.private_urls, []) : url["url"]], local.additional_private_urls)
+  value       = local.private_urls
   description = "list(string) ||| A list of URLs only accessible inside the network"
 }
 
 output "public_urls" {
-  value       = concat([for url in try(local.capabilities.public_urls, []) : url["url"]], local.additional_public_urls)
+  value       = local.public_urls
   description = "list(string) ||| A list of URLs accessible to the public"
 }
