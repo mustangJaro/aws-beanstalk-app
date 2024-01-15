7085a9f07f2b1c125c28bc8c2d6c2052ee2b8c34
AJ <ajarosinski@gmail.com>
2024-01-15 15:42:16 -0600

    Update instance profile policy and add adminer

--- role.tf
+++ role.tf
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "this" {
 // This policy enables SSM on the instance
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.this.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2RoleforSSM"
 }
 
 resource "aws_iam_role_policy_attachment" "docker" {
